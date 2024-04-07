e7110d023610013e2715e71ad6b25ddc28b34e19
kphed <kpcrypto69@gmail.com>
2024-04-07 19:34:09 -0400

    Add MinerETH deployment script

--- script/MinerETH.s.sol
+++ script/MinerETH.s.sol
@@ -6,14 +6,17 @@ import {MinerETHFactory} from "src/MinerETHFactory.sol";
 import {MinerETH} from "src/MinerETH.sol";
 
 contract MinerETHScript is Script {
-    MinerETHFactory public constant FACTORY = MinerETHFactory(address(0));
-    uint256 public constant SEED_VALUE = 0.01 ether;
+    MinerETHFactory public constant FACTORY =
+        MinerETHFactory(0x5d3785F47617fE1d3dfbfBB03a5ada475D534CCf);
+    uint256 public constant SEED_VALUE = 0.02 ether;
+    uint256 public constant DEAD_VALUE = 0.01 ether;
+    address public constant DEAD = address(0xdead);
     address public constant ELON = 0xAa6Cccdce193698D33deb9ffd4be74eAa74c4898;
     address public constant NFD = 0x37289326b7Bca5a776A5071b8d693C0588c5C9A6;
     address public constant DEGEN = 0x4ed4E862860beD51a9570b96d89aF5E1B0Efefed;
 
     function run() public {
-        vm.broadcast(vm.envUint("PRIVATE_KEY"));
+        vm.startBroadcast(vm.envUint("PRIVATE_KEY"));
 
         // Deploy, initialize, and seed each vault with "dead shares" to deter dos'ing.
         MinerETH elonMiner = MinerETH(payable(FACTORY.deploy(ELON)));
@@ -23,5 +26,12 @@ contract MinerETHScript is Script {
         elonMiner.deposit{value: SEED_VALUE}("");
         nfdMiner.deposit{value: SEED_VALUE}("");
         degenMiner.deposit{value: SEED_VALUE}("");
+
+        // Burn shares to ensure there's always interest accruing to offset Comet rounding.
+        elonMiner.transfer(DEAD, DEAD_VALUE);
+        nfdMiner.transfer(DEAD, DEAD_VALUE);
+        degenMiner.transfer(DEAD, DEAD_VALUE);
+
+        vm.stopBroadcast();
     }
 }
